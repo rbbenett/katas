95c17ee35bab1809fa68138aeda66777891ec595
Robert <rbbenett0614@gmail.com>
2020-11-08 19:18:58 +0000

    Case Maker Problem

---
+++ kata7-stretch.js
@@ -0,0 +1,9 @@
+const camelCase = function(input) {
+  return input.replace(/\W+(.)/g, function(match, chr)
+  {
+       return chr.toUpperCase();
+   });
+};
+console.log(camelCase("this is a string"));
+console.log(camelCase("loopy lighthouse"));
+console.log(camelCase("supercalifragalisticexpialidocious"));
